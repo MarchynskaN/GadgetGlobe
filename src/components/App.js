ea28cf3d126dbe8121fd9fe0a50a958e5901fede
MarchynskaN <119426243+MarchynskaN@users.noreply.github.com>
2023-07-10 16:20:05 -0700

    Registration Component July 10th
    1. added registration and NotFound error components
    2. changed folders structure accordingly

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import '../css/App.css';
 import Header from './Header';
 import Profile from './Profile';
-
+// @ts-ignore
+import Registration from './Registration';
+import NotFound from './NotFound';
 import Home from './Home';
+
 const App = (props) => {
   return (
     <Router>
@@ -16,38 +18,12 @@ const App = (props) => {
           <Route path="/" element={<Home/>}/>
           {/* <Route path="/" element={<Home  gadgetsdetail={props.gadgetsdetail}  />} /> */}
           <Route path="/profile" element={<Profile />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
   );
 };
-// class App extends React.Component{
-//   state={
-// gadgets:this.props.gadgets
-//   };
-//   render(){
-//     return(  
-      
-//       <div>
-//       <Header1/>
-
-
-//       {/* {this.state.gadgets.map((gadget)=>(
-//        <Body gadgetdetail={gadget}/>
-//       ))}
-//       */}
-//       </div>
-     
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
 
 export default App;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import './index.css';
+import React, { Component} from 'react';
+import {NavLink } from 'react-router-dom';
+
 class Profile extends Component {    
   
   render() {
     return (
       <div className='profile'>
-
         <h1>Sign In</h1>
         <label>Email address</label><br/>
         <input type='text'/>
-     <br/><br/><br/>
-     <label>Password</label><br/>
+        <br/><br/><br/>
+        <label>Password</label><br/>
         <input type='password'/>
         <br/><br/><br/>
-       <button type='submit'>Sign In</button>
+        <button type='submit'>Sign In</button>
+          <NavLink to="/registration"><span>Create an account &gt;</span></NavLink>
       </div>
     );
   }
